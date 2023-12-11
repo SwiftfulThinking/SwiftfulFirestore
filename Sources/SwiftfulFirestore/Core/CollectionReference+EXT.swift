4ff51b23d847e31dcc11efbbde857de61d96d919
Nick <hello@swiftful-thinking.com>
2023-12-10 19:15:38 -0500

    setDocument from dict

--- Sources/SwiftfulFirestore/Core/CollectionReference+EXT.swift
+++ Sources/SwiftfulFirestore/Core/CollectionReference+EXT.swift
@@ -22,6 +22,11 @@ public extension CollectionReference {
         try await self.document(document.id).setData(dict, merge: true)
     }
     
+    /// Create or overwrite document. Merge: TRUE
+    func setDocument(id: String, dict: [String:Any]) async throws {
+        try await self.document(id).setData(dict, merge: true)
+    }
+    
     /// Update existing document.
     func updateDocument<T:Codable>(id: String, document: T) async throws {
         let dict = try document.asJsonDictionary()
